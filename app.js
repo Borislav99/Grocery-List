de6e1f621ac4f290098c4f1d4ff24ef2fc369885
Borislav Adamovic <borislavadamovic9@gmail.com>
2020-04-19 19:34:32 +0200

    app.js

--- app.js
+++ app.js
@@ -54,6 +54,7 @@
       };
       id++;
       this.addToDOM(object);
+      this.addToArray(object);
     }
     addToDOM(object) {
       let div = document.createElement("div");
@@ -78,9 +79,58 @@
      `;
       taskList.appendChild(div);
     }
-    checkIcon(icon, text){
-     text.classList.toggle('strike');
-     icon.classList.toggle('low');
+    checkIcon(icon, text) {
+      text.classList.toggle("strike");
+      icon.classList.toggle("low");
+    }
+    removeIcon(value, id) {
+      taskList.removeChild(value);
+      objectsList = objectsList.filter(function (item) {
+        return item.id !== id;
+      });
+    }
+    editIcon(text) {
+      this.hideTask();
+      this.openInput();
+      input.value = text;
+      submit.value = "Edit Grocery";
+      submit.removeEventListener("click", this.submitEvent);
+      submit.addEventListener("click", this.editEvent);
+      this.showBtn();
+    }
+    editEvent() {
+     let id;
+      let ui = new UI();
+      let arr = [];
+      objectsList.forEach((item) => {
+        arr.push(item.id);
+      });
+      if (arr.length === 0) {
+        id = 0;
+      } else {
+        id = Math.max(...arr) + 1;
+      }      
+      let text = input.value;
+      let object = {
+        name: text,
+        id: id,
+      };
+      ui.addToArray(object);
+      ui.addToDOM(object);
+      addTask.classList.add("show");
+      ui.closeInput();
+      submit.removeEventListener("click", ui.editEvent);
+      submit.addEventListener("click", ui.submitEvent);
+      submit.value = "Add Grocery";
+    }
+    addToArray(object) {
+      objectsList.push(object);
+    }
+    submitEvent(event) {
+      let value = input.value;
+      ui.hideBtn();
+      ui.closeInput();
+      ui.addToList(value);
     }
   }
   //varijable
@@ -93,6 +143,7 @@
   let inputContainer = document.querySelector(".input-container");
   let taskList = document.querySelector(".taskList");
   let id = 0;
+  let objectsList = [];
   //eventi
   let ui = new UI();
   document.addEventListener("DOMContentLoaded", (event) => {
@@ -114,22 +165,23 @@
       ui.hideBtn();
     }
   });
-  submit.addEventListener("click", (event) => {
-    let value = input.value;
-    ui.hideBtn();
-    ui.closeInput();
-    ui.addToList(value);
-  });
+  submit.addEventListener("click", ui.submitEvent);
   taskList.addEventListener("click", (event) => {
     event.preventDefault();
-    let value = event.target.parentElement.parentElement.parentElement.parentElement.children[0].children[0];
+    let value =
+      event.target.parentElement.parentElement.parentElement.parentElement
+        .children[0].children[0];
     let textValue = value.textContent;
+    let id = parseInt(event.target.parentElement.dataset.id);
+    let singleItem =
+      event.target.parentElement.parentElement.parentElement.parentElement;
     if (event.target.parentElement.classList.contains("checkIcon")) {
-     ui.checkIcon(event.target, value)
+      ui.checkIcon(event.target, value);
     } else if (event.target.parentElement.classList.contains("editIcon")) {
-      console.log(`edit`);
+      ui.removeIcon(singleItem, id);
+      ui.editIcon(textValue, id);
     } else if (event.target.parentElement.classList.contains("removeIcon")) {
-      console.log(`remove`);
+      ui.removeIcon(singleItem);
     }
   });
 })();
